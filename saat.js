cc1d26f4dfadc86caa56ee57f67f08148801e60c
= <=>
2022-07-22 18:02:29 +0300

    Saatin HTML CSS ve JAVASCRİPT kodları eklendi.

---
+++ saat.js
@@ -0,0 +1,12 @@
+const sec =document.querySelector(".secs");
+const min= document.querySelector(".mins");
+const hours = document.querySelector(".hours");
+function tiktak(){
+    let hour =new Date().getHours();
+    let munite=new Date().getMinutes();
+    let second= new Date().getSeconds();
+    sec.style.transform = `rotate(${180 + (second * 6)}deg)`
+    min.style.transform=`rotate(${180+(munite*6)}deg)`
+    min.style.transform=`rotate(${180+(hours*30)}deg)`
+}
+setInterval(tiktak, 1000);
